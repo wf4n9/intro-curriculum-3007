6e98835b6f6f8767093c17fe45e4e19e5c980679
Shinri Fujikawa <shinri.fj@gmail.com>
2017-10-19 10:00:56 +0000

    seems ok

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/**
+* 17の倍数かどうかを判定する
+* @n {number}
+*/
+
+function isMultipleOfSeventeen(n) {
+	return n % 17 === 0;
+}
+
+
 module.exports = {
+	isMultipleOfSeventeen: isMultipleOfSeventeen
 }
